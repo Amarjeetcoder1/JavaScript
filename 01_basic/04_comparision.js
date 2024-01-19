dae163a3ce979ac1d8407f917eac57ffa0265aa4
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-19 17:40:53 +0000

    Comparsion and Data Types Summary

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1); //"2" convert to integer,always use same data type
+console.log("02" > 1);
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0); //This null convert to 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+console.log("2" === 2);
+
+//********NOTES****** *//
+//Always use same data type comparsion
---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,25 @@
+// Data Types are categorised mainly in two ways:
+// 1 Primitive
+
+// Primative Data types are also called call by value, it's means it take the copied valued from the memory not thr original value.
+// 7 types: String, Number, Boolean, Null, Undefinrd, BigInt, Symbol
+
+const score = 100;
+const scoreValue = 102.0;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol ('1321')
+const anotherId = Symbol ('1321')
+
+console.log(id == anotherId);
+
+const bigNumber = 323234334553353534n
+
+//2. Reference (Non primitive)
+
+//Array, Objects, Functions
+
+ 
