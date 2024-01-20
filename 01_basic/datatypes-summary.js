484ac06bc80a0258a45c1c2a78ca5b15b559bdaa
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-20 06:06:16 +0000

    Data Types summary

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -1,7 +1,7 @@
 // Data Types are categorised mainly in two ways:
 // 1 Primitive
 
-// Primative Data types are also called call by value, it's means it take the copied valued from the memory not thr original value.
+// Primative Data types are also called CALL BY VALUE, it's means it take the copied valued from the memory not thr original value.
 // 7 types: String, Number, Boolean, Null, Undefinrd, BigInt, Symbol
 
 const score = 100;
@@ -21,5 +21,21 @@ const bigNumber = 323234334553353534n
 //2. Reference (Non primitive)
 
 //Array, Objects, Functions
+// All this Non-Primative have typeof: 'Object or fun object'
 
- 
+const heros = ["Shaktiman", "naagraj", "doga"];
+console.log(heros);
+
+let users = {
+    "name": "Amy Garcia",
+    "email": "amygarcia@example.com",
+    "phone": "6543219870"
+  }
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+console.log(typeof(outsideTemp));
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
