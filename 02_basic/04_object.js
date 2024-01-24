1d8ea8540ca2a296f7b76912dae70b98a86f03d2
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-24 18:17:52 +0000

    Array of object

--- 02_basic/04_object.js
+++ 02_basic/04_object.js
@@ -1,7 +1,8 @@
 // Singleton
 
-const tinderUser = new Object()
+// const tinderUser = new Object()
 
+const tinderUser = {}
 tinderUser.id = "amarjeet123"
 tinderUser.name = "Amarjeet"
 tinderUser.age = 18
@@ -9,9 +10,12 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
+//Object 1
 const regularUser = {
     email: "Amarjeet123@gmail.com",
+    //object 2
     fulltname: {
+        //object 3
         userfullname: {
             firstname: "Aman",
             lastname: "Ghosh"
@@ -19,4 +23,86 @@ const regularUser = {
     }
 }
 
+console.log(regularUser.fulltname.userfullname);
+
 // console.log(regularUser.fulltname.userfullname.firstname);
+
+const obj1 = { 1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {4: "a", 5: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign( {}, obj1, obj2, obj4)
+// console.log(obj3);
+
+// ++++++++++++NOTES++++++++++++++
+// target => {}
+// Source => obj1, obj2, obj3
+
+//Simple way to merage two array
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+// ++++Array of Objects ++++++
+
+const users = [
+    {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      phone: "1234567890"
+    },
+    {
+      name: "Jane Smith",
+      email: "janesmith@example.com",
+      phone: "9876543210"
+    },
+    {
+      name: "Mike Johnson",
+      email: "mikejohnson@example.com",
+      phone: "4567891230"
+    },
+    {
+      name: "Emily Davis",
+      email: "emilydavis@example.com",
+      phone: "7891234560"
+    },
+    {
+      name: "Chris Wilson",
+      email: "chriswilson@example.com",
+      phone: "3216549870"
+    },
+    {
+      name: "Sarah Thompson",
+      email: "sarahthompson@example.com",
+      phone: "6549873210"
+    },
+    {
+      name: "David Anderson",
+      email: "davidanderson@example.com",
+      phone: "9873216540"
+    },
+    {
+      name: "Amy Garcia",
+      email: "amygarcia@example.com",
+      phone: "6543219870"
+    },
+    {
+      name: "Mark Lee",
+      email: "marklee@example.com",
+      phone: "3219876540"
+    },
+    {
+      name: "Laura Hernandez",
+      email: "laurahernandez@example.com",
+      phone: "9876543210"
+    }
+  ]
+
+  users[1].email
+  console.log(tinderUser);
+
+  console.log(Object.keys(tinderUser));
+  console.log(Object.values(tinderUser));
+  console.log(Object.entries(tinderUser));
+
+  console.log(tinderUser.hasOwnProperty('isLoggedIn'));
