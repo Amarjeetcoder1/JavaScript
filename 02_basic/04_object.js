536c62dab82158ee2361bab9de00e9303b8e2ca7
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-24 17:29:32 +0000

    Some more about Object

---
+++ 02_basic/04_object.js
@@ -0,0 +1,22 @@
+// Singleton
+
+const tinderUser = new Object()
+
+tinderUser.id = "amarjeet123"
+tinderUser.name = "Amarjeet"
+tinderUser.age = 18
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "Amarjeet123@gmail.com",
+    fulltname: {
+        userfullname: {
+            firstname: "Aman",
+            lastname: "Ghosh"
+        }
+    }
+}
+
+// console.log(regularUser.fulltname.userfullname.firstname);
