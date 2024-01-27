3ad05fc4e3efd15e88c656246a8f4d36c0804679
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-27 15:50:33 +0000

    This and arrow function in JS

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,82 @@
+const user = {
+    username: "Amarjeet",
+    price: 999,
+
+    welcomeMessage: function() {
+    //This key word is use for refers or access the current conntext like username, price within the scope 
+        console.log(`${this.username}, welcome to website`);
+        // console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"   // Here we change the context mean's value
+// user.welcomeMessage()
+// console.log(this);
+
+//++++++++++++++++++++++++++++++++++++++
+
+
+// function chai () {
+//     let username = "Aman"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function() {
+//     let username = "Aman"
+//     console.log(this.username);
+// }
+// chai()
+
+
+// Decleration of function in Two Types:
+//1
+function Userlogined() {
+    const username = "Hitesh"
+    // console.log(username)
+}
+Userlogined()
+
+//2
+const NewUserlogined = function () {
+    const username = "Aman sinha"
+    // console.log(username);
+}
+NewUserlogined()
+
+
+//+++++++++++++++ Arrow Function ++++++
+// Syntax:-
+const chai = () => {
+    let username = "Aman"
+   // console.log(this);
+}
+chai()
+
+// First way to declear Arrow Function
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// NOTE:- Curly braces ma rap kiya than use return key word
+
+//Second way to declear Arrow Function
+const addTwo = (num1, num2) =>  (num1 + num2)
+
+// NOTE:- Parentheses braces ma rap kiya than don't use return key word
+
+
+console.log(addTwo(2,8));
+
+// Declear an object in Arrow function
+//const addTwo = (num1, num2) =>  ({username: "Amarjeet"})
+
+
+// Simple Example
+
+// const myArray = [2, 4, 7, 8]
+
+// myArray.forEach(() => (num1,num2))
