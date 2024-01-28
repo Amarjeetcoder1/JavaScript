31a4fddacd6a5e4bd0cca0213706f1151b4a2027
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-28 14:52:31 +0000

    If else and Switch control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,74 @@
+//if
+// if (true) {
+//     // Code will execute
+// }
+
+// if(false) {
+//     //code will not execute
+// }
+
+// <, >, <=, >=, ==, !=, !==, ===
+const isUserLoggedIn = true
+
+if (2 !== "2") {
+    // console.log("Executed");
+}
+else {
+    //console.log("Not Execute");
+}
+
+
+
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("Temperature less than 50");
+// }
+// else {
+//     console.log("Temperature greather than 50");
+// }
+// console.log("Succesfully Executed");
+
+
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); myzer 
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const UserLoggedIn = true
+const debitcard = true
+const loggInFromGoogle = false
+const loggInFromEmail = true
+
+if (UserLoggedIn && debitcard && 2==2) {
+    console.log("Allow to buy course");
+}
+
+if (loggInFromGoogle || loggInFromEmail) {
+    console.log("User Logged in");
+}
+
