2f3fe88f9d5538236fa93415b2fe5c4b17db24bd
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-28 06:30:59 +0000

    IIFE in JavaScript

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,31 @@
+// Immediately Invoked Function Expressionsb (IIFE)
+//Syntax:-
+
+// (function(){
+//     //...
+// })();
+
+
+
+
+(function chai() {
+    // named IIFE
+    console.log("DB CONNECTED");
+}) ();
+
+// chai()
+
+// () => The first one is use for Function Defination
+// () => The second one is use for execution
+
+(() => {
+    // Simple IIFE
+    console.log("DB TWO CONNECTED");
+})();
+
+const user = ( (name) => (
+    console.log(`DB THREE CONNECTED ${name}`)
+)) ("Amarjeet")
+
+
+
