5f26bbb459f3d2c1ea333bfd39d92010b07a1441
Amarjeet Ghosh <90110541+Amarjeetcoder1@users.noreply.github.com>
2024-01-29 16:46:15 +0000

    For loop in Java Script

---
+++ 05_iterations/one.js
@@ -0,0 +1,35 @@
+//for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(i);
+
+for (let i = 1; i <= 20; i++) {
+   // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value: ${j} and inner loop ${i}`);
+      // console.log(i + '*' + j + ' = '+ i*j);
+    }       
+}
+
+const myarray = ["Flash", "Batman", "Superman"]
+// console.log(myarray.length);
+for (let index = 0; index < myarray.length; index) {
+    const element = myarray[index];
+    // console.log(element);
+    
+}
+
+// break and continue 
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
